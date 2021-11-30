4169ba479cf00b0185748de9c8592f3bdd825e84
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-11-30 20:53:57 -0300

    Codificacion en hemming y limpieza en reg a escribir

--- ProyMicroAlonsoPeiranoMartinez/main.asm
+++ ProyMicroAlonsoPeiranoMartinez/main.asm
@@ -35,28 +35,102 @@ start:
 ; Replace with your application code
 main:
     nop
-	nop
-	nop
+	call delay ;le da tiempo al random int de generar un numero significativamente distinto del anterior
+	call cargarEnRam
 	nop
     rjmp main
 
 randomBit_int: ;se genera numero random en r17
 
 	in r20, SREG ;guardo las flags por las dudas.
-	clr r16
 	lds r16, ADCL ;cargo los 8 bits menos sinigficativos
 	
 	sbrc r16,0
 	inc r17
 	lsl r17	;muevo los bits a la izquierda
 
+	lds r16, ADCH ;cargo adch para que se pueda actualizar el adc, no lo uso.
+
 	out SREG, r20
 	reti
 
 cargarEnRam:
 
-	mov	r16,r17
-	st	Y, r16
-	;inc Y
+	mov	r16,r18
+	st	Y+, r16
 
+	ret
 
+codificarHemming:
+	mov r18,r17
+	clr r22
+
+	Chckr1:
+		sbrc r18,3
+		inc r22
+		sbrc r18,5
+		inc r22
+		sbrc r18,7
+		inc r22
+		cpi r22,0
+		breq clrbit1
+		cpi r22,2
+		breq clrbit1
+		sbr r18,1
+		clr r22
+		rjmp Chckr2
+
+		clrbit1:
+		cbr r18,1
+		clr r22
+		rjmp Chckr2
+
+	Chckr2:
+		sbrc r18,3
+		inc r22
+		sbrc r18,6
+		inc r22
+		sbrc r18,7
+		inc r22
+		cpi r22,0
+		breq clrbit2
+		cpi r22,2
+		breq clrbit2
+		sbr r18,2
+		clr r22
+		rjmp Chckr4
+
+		clrbit2:
+		cbr r18,2
+		clr r22
+		rjmp Chckr4
+
+	Chckr4:
+		sbrc r18,5
+		inc r22
+		sbrc r18,6
+		inc r22
+		sbrc r18,7
+		inc r22
+		cpi r22,0
+		breq clrbit4
+		cpi r22,2
+		breq clrbit4
+		sbr r18,4
+		cbr r18,7
+ret
+
+		clrbit4:
+		cbr r18,4
+		cbr r18,7
+ret
+
+delay:
+	inc r20
+	brne delay
+	delay2:
+		inc r21
+		brne delay2
+	clr r21
+	clr r20
+	ret
